c3d1cfff266419dc419d8e9d13302a8818178703
Douglastm <douglasteixeiramagalhaes@gmail.com>
2026-06-29 23:31:18 -0300

    feat(home): Create dashboard home page

---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './HomePage.module.css';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className={styles.page}>
+      <h1>Home Sales System</h1>
+    </div>
+  );
+};
+
+export default HomePage;
